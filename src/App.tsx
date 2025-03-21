bf875140bbdd2e3f166b422793b2cd92ed11a3c8
dameiso412 <damaso@sacamedi.com>
2025-03-21 17:06:16 -0300

    Update src/App.tsx - 2025-03-21T20:06:00.379Z

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 
-// No React import needed for modern TypeScript React with JSX transform
-import LandingPage from './LandingPage';
+import './App.css'
+import LandingPage from './LandingPage'
 
 function App() {
-  // Updated at: 2025-03-21T19:42:52.016Z
-  // Update ID: 66am1153
+  // This component was last updated at: 2025-03-21T20:06:00.379Z
   return (
     <div className="App">
       <LandingPage />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
