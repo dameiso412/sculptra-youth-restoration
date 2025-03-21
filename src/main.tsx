647706abcacfa5f1cc7a0880d53accc35efd7eee
dameiso412 <damaso@sacamedi.com>
2025-03-21 17:27:08 -0300

    Update src/main.tsx - 2025-03-21T20:26:14.294Z

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// Updated at: 2025-03-21T20:06:00.379Z
+// Updated at: 2025-03-21T20:26:14.294Z
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
