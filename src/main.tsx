6cae35f1778e713809ba8f1705126656f36d27f9
dameiso412 <damaso@sacamedi.com>
2025-03-21 17:35:38 -0300

    Fix src/main.tsx for proper rendering - 2025-03-21T20:35:26.693Z

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// Updated at: 2025-03-21T20:26:14.294Z
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
