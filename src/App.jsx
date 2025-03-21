8845bd45f7760994ab90dd3c162af45d93832900
dameiso412 <damaso@sacamedi.com>
2025-03-21 16:26:02 -0300

    Update App to use landing page - 2025-03-21T19:25:43.442Z

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react'
+import LandingPage from './LandingPage'
+
+function App() {
+  // Updated at: 2025-03-21T19:25:43.442Z
+  // Update ID: f5sxucqs
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
