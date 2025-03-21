affecd43c1b10c7d864a8fc83983d734ec5c7164
dameiso412 <damaso@sacamedi.com>
2025-03-21 17:06:22 -0300

    Update App to use landing page - 2025-03-21T20:06:00.379Z

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 
-import './App.css'
-import LandingPage from './LandingPage'
+import LandingPage from './LandingPage';
+import './App.css';
 
 function App() {
-  // This component was last updated at: 2025-03-21T20:06:00.379Z
+  // Updated at: 2025-03-21T20:06:00.379Z
+  // Update ID: 6i8zhpjh
   return (
     <div className="App">
       <LandingPage />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
