dff18fe16c6a50d88b9dff8e3e89aacb6e4f5a6a
dameiso412 <damaso@sacamedi.com>
2025-03-21 17:49:10 -0300

    Fix App component for proper rendering - 2025-03-21T20:48:21.399Z

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import LandingPage from './LandingPage';
 import './App.css';
 
 function App() {
-  // Updated at: 2025-03-21T20:48:18.664Z
+  // Updated at: 2025-03-21T20:48:21.399Z
   return (
     <div className="App">
       <LandingPage />
